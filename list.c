c41cb13d6a68f437ed75bf35806839018ef845f6
H. Peter Anvin <hpa@zytor.com>
2007-04-02 17:48:42 -0700

    Propagate PRI*N,SCN*N everywhere (3b / Type handling cleanup)
    this subphase:  Fix all of the printf/scanf format strings.
    
    !(- use C99-like macros for printf)
    !(- use [u]int*_t types (autoconf can verify they exist))
    - abstract Num and Objid out
    - default to 64 bit integers
    !(- un-pointerify floating-point numbers (makes no sense on modern hardware))
    
    - - from james
    
    Kenny's fixes to get it to load Minimal.db and JHCore. (1)
    
    (evidently Kenny found two PRI*N goofs in db_file.c  --wrog)

--- list.c
+++ list.c
@@ -238,10 +238,10 @@ stream_add_tostr(Stream * s, Var v)
 {
     switch (v.type) {
     case TYPE_INT:
-	stream_printf(s, "%d", v.v.num);
+	stream_printf(s, "%"PRIdN, v.v.num);
 	break;
     case TYPE_OBJ:
-	stream_printf(s, "#%d", v.v.obj);
+	stream_printf(s, "#%"PRIdN, v.v.obj);
 	break;
     case TYPE_STR:
 	stream_add_string(s, v.v.str);
@@ -277,10 +277,10 @@ unparse_value(Stream * s, Var v)
 {
     switch (v.type) {
     case TYPE_INT:
-	stream_printf(s, "%d", v.v.num);
+	stream_printf(s, "%"PRIdN, v.v.num);
 	break;
     case TYPE_OBJ:
-	stream_printf(s, "#%d", v.v.obj);
+	stream_printf(s, "#%"PRIdN, v.v.obj);
 	break;
     case TYPE_ERR:
 	stream_add_string(s, error_name(v.v.err));
