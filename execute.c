e2905809dfc733929f33c5c6bcde123d12b7c6d3
H. Peter Anvin <hpa@zytor.com>
2007-04-02 17:48:42 -0700

    db_io issues (1b / Type handling cleanup)
    (phase 1 = changes having nothing to do with introducing [u]int*_t,
    missing headers and adding/fixing function prototypes,
    Num-introduction, float-unboxing, autoconf, or the new math builtins
    
    here: everything having to do with db file reading/writing
     --wrog)
    
    was:
    - use C99-like macros for printf
    - use [u]int*_t types (autoconf can verify they exist)
    - abstract Num and Objid out
    - default to 64 bit integers
    - un-pointerify floating-point numbers (makes no sense on modern hardware)

--- execute.c
+++ execute.c
@@ -2744,7 +2744,7 @@ write_activ_as_pi(activation a)
 int
 read_activ_as_pi(activation * a)
 {
-    int dummy;
+    Objid dummy;
     char c;
 
     free_var(dbio_read_var());
@@ -2883,6 +2883,7 @@ int
 read_activ(activation * a, int which_vector)
 {
     DB_Version version;
+    unsigned int v;
     Var *old_rt_env;
     const char **old_names;
     unsigned old_size, stack_in_use;
@@ -2893,10 +2894,10 @@ read_activ(activation * a, int which_vector)
 
     if (dbio_input_version < DBV_Float)
 	version = dbio_input_version;
-    else if (dbio_scanf("language version %u\n", &version) != 1) {
+    else if (dbio_scanf("language version %u\n", &v) != 1) {
 	errlog("READ_ACTIV: Malformed language version\n");
 	return 0;
-    } else if (!check_db_version(version)) {
+    } else if (version = v, !check_db_version(version)) {
 	errlog("READ_ACTIV: Unrecognized language version: %d\n",
 	       version);
 	return 0;
