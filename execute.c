0dc74657c05d108d81f5cd239ad29dfd83ee4ce0
Roger Crew <crew@cs.stanford.edu>
2022-09-27 17:50:08 -0700

    suppress warnings re monster opcode switch in run()

--- execute.c
+++ execute.c
@@ -804,6 +804,8 @@ do {    						    	\
 		return OUTCOME_ABORTED;
 	    }
 	}
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch"
 	switch (op) {
 
 	case OP_IF_QUES:
@@ -2098,6 +2100,7 @@ do {    						    	\
 		panic("Unknown opcode!");
 	    break;
 	}
+#pragma GCC diagnostic pop
     }
 }
 
