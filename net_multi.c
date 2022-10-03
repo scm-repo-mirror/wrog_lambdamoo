d37e1fb8ea84736625376ae62e62537b47d9fb0b
Roger Crew <crew@cs.stanford.edu>
2022-10-02 22:07:00 -0700

    signed-vs-unsigned (net_multi)
    proto.pocket_size is unsigned
    freebsd/clang complained about telnet_cmd

--- net_multi.c
+++ net_multi.c
@@ -383,7 +383,7 @@ make_new_connection(server_listener sl, int rfd, int wfd,
 static void
 get_pocket_descriptors(void)
 {
-    int i;
+    unsigned i;
 
     if (!pocket_descriptors)
 	pocket_descriptors =
@@ -403,7 +403,8 @@ accept_new_connection(nlistener * l)
 {
     network_handle nh;
     nhandle *h;
-    int rfd, wfd, i;
+    int rfd, wfd;
+    unsigned i;
     const char *host_name;
 
     switch (proto_accept_connection(l->fd, &rfd, &wfd, &host_name)) {
@@ -645,7 +646,7 @@ network_set_client_echo(network_handle nh, int is_on)
 #define TN_WONT	252
 #define TN_ECHO	1
 
-    static char telnet_cmd[4] =
+    static unsigned char telnet_cmd[4] =
 	{TN_IAC, 0, TN_ECHO, 0};
 
     h->client_echo = is_on;
@@ -653,7 +654,7 @@ network_set_client_echo(network_handle nh, int is_on)
 	telnet_cmd[1] = TN_WONT;
     else
 	telnet_cmd[1] = TN_WILL;
-    enqueue_output(nh, telnet_cmd, 3, 0, 1);
+    enqueue_output(nh, (char *)telnet_cmd, 3, 0, 1);
 }
 
 #else /* NETWORK_PROTOCOL == NP_SINGLE */
