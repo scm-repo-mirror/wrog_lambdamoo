7b9456cb37e7b842aaf29211a9e10e3ad7d48211
Roger Crew <crew@cs.stanford.edu>
2023-08-24 20:39:01 -0700

    use (s)size_t types for streams

--- streams.c
+++ streams.c
@@ -27,7 +27,7 @@
 #include "utf.h"
 
 Stream *
-new_stream(int size)
+new_stream(size_t size)
 {
     Stream *s = mymalloc(sizeof(Stream), M_STREAM);
 
@@ -42,7 +42,7 @@ new_stream(int size)
 }
 
 static void
-grow(Stream * s, int newlen)
+grow(Stream * s, size_t newlen)
 {
     char *newbuf;
 
@@ -96,10 +96,10 @@ stream_delete_utf(Stream * s)
 }
 
 void
-stream_add_bytes(Stream * s, const char *bytes, int len)
+stream_add_bytes(Stream * s, const char *bytes, size_t len)
 {
     if (s->current + len >= s->buflen) {
-	int newlen = s->buflen * 2;
+	size_t newlen = s->buflen * 2;
 
 	if (newlen <= s->current + len)
 	    newlen = s->current + len + 1;
@@ -113,7 +113,7 @@ void
 stream_printf(Stream * s, const char *fmt,...)
 {
     va_list args, pargs;
-    int len;
+    ssize_t len;
 
     va_start(args, fmt);
 
@@ -122,7 +122,7 @@ stream_printf(Stream * s, const char *fmt,...)
     va_end(pargs);
 
     if (s->current + len >= s->buflen) {
-	int newlen = s->buflen * 2;
+	size_t newlen = s->buflen * 2;
 
 	if (newlen <= s->current + len)
 	    newlen = s->current + len + 1;
@@ -155,7 +155,7 @@ stream_contents(Stream * s)
     return s->buffer;
 }
 
-int
+size_t
 stream_length(Stream * s)
 {
     return s->current;
