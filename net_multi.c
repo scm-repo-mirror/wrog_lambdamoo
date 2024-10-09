d548ae6d74830692f31c0219b720b8d1b9f86256
Roger Crew <crew@cs.stanford.edu>
2024-10-09 00:47:06 -0700

    binary->moobinary
    moobinary_to_raw_bytes <- binary_to_raw_bytes
    raw_bytes_to_moobinary <- raw_bytes_to_binary
    
    I believe 'binary' is confusing in this context
    since it refers to the format for MOO code strings in
    which nonprintable characters are represented as ~NNN
    (and NOT, say, strings like "011010001010101011010")
    
    The change to 'moobinary' is to alert people to this
    specialized meaning (it would be nice to have a name other than
    'binary' for this, but we're rather stuck at this point).

--- net_multi.c
+++ net_multi.c
@@ -277,7 +277,7 @@ pull_input(nhandle * h)
 
     if ((count = read(h->rfd, ptr, sizeof(buffer) - h->excess_utf_count)) > 0) {
 	if (h->binary) {
-	    stream_add_string(s, raw_bytes_to_binary(buffer, count));
+	    stream_add_string(s, raw_bytes_to_moobinary(buffer, count));
 	    server_receive_line(h->shandle, reset_stream(s));
 	    h->last_input_was_CR = 0;
             h->excess_utf_count = 0;
