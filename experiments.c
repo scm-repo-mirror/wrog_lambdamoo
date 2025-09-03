86c39b8f92f4934e453c4cedc983c65e54b4ce6c
Roger Crew <crew@cs.stanford.edu>
2025-09-02 19:31:02 -0700

    fix experiments.c, add experiments.h
    defining EXAMPLE to 1 should produce something that compiles.
    Also this is a chance to set style guidelines for headers.

--- experiments.c
+++ experiments.c
@@ -33,33 +33,83 @@
  * its #ifdef-ed out body that *does*, in fact, get called on every
  * server startup even though it does nothing by default (i.e.,
  * assuming you leave it unmodified).
- *
- * (note: This file *was* originally named extensions.c, but has since
- *  been renamed to better reflect its role as a playground now that
- *  (25 years later) there is a somewhat more developed extension
- *  framework and with more thought put into how extensions should be
- *  defined, behave, and interact with each other in the server
- *  context.  See extensions.txt for what you need to know once you
- *  are ready to migrate code *out* of this file and into an actual
- *  extension.)
  */
 
+/*\ Random explanatory meta-comments that do not need to appear
+ *| in any Actual File will look like this.  Enjoy.  --wrog
+ */
+
+/*\ (Historical note:  This file *was* originally named extensions.c,
+ *|  but has since been renamed to better reflect its role as a
+ *|  playground now that (25 years later) there is a somewhat more
+ *|  developed extension framework and with a bit more thought put
+ *|  into how extensions should be defined, behave, and interact with
+ *|  each other in the server context.  See Extensions.md for what you
+ *|  need to know once you are ready to migrate code *out* of this
+ *|  file and into an actual extension.)
+ */
+
+/*\ 1st block of includes for a .c file answers this question:
+ *| What interfaces are we exporting or contributing to?
+ *|
+ *| As per usual C practice, declare anything of use to other modules
+ *| in experiments.h.  And usually there will just be one file here.
+ *|
+ *| However, because *this* file also defines a registration function
+ *| (register_experiments()), and, as a matter of convention and unlike
+ *| all other interfaces, the registration functions are all "owned" by
+ *| the bf_register module (even though they do not actually live in
+ *| bf_register.c; see explanation in bf_register.h), there will, in
+ *| fact be a 2nd file to mention here:
+ */
+#include "experiments.h"
 #include "bf_register.h"
 
+/*\ The following is something that only *this* file does because
+ *| we are blocking out everything that actually does anything.
+ */
 #define EXAMPLE 0
-
-#include "functions.h"
-
 #if EXAMPLE
 
+/*\ 2nd block of includes for a .c file:  config.h and options.h
+ *|
+ *| Yes the headers would have included them if they needed them, but
+ *| maybe they didn't.  Headers tend to be conservative about this.
+ *| It is, however, pretty much guaranteed you'll eventually reference
+ *| something from there, so you may as well put these in now and get
+ *| it over with:
+ */
+#include "config.h"
+#include "options.h"
+
+/*\ 3rd block of includes for a .c file:  System-corrective headers.
+ *| Many of these are explained in the various stanzas of config.h.in
+ */
 #include "my-unistd.h"
 
+/*\ 4th block of includes for a .c file:
+ *| Actual headers needed from other parts of the server.
+ *| Alphbetical order is fine (see experiments.h comment).
+ */
 #include "exceptions.h"
+#include "functions.h"
 #include "log.h"
+#include "network.h"
 #include "net_multi.h"
 #include "storage.h"
 #include "tasks.h"
 
+/*\ Unless you're doing something funky (like Pavel did with the networking .h files)
+ *| do NOT bury any #includes further down the file where they
+ *|   (1) will NOT be seen, and
+ *|   (2) might possibly make code in front of them behave differently
+ *|       because they haven't been #included yet,
+ *|       which will be super-mysterious to debug,
+ *|       and whoever gets stuck doing that will hate you forever.
+ */
+
+/*\ Begin actual content of the file:
+ */
 typedef struct stdin_waiter {
     struct stdin_waiter *next;
     vm the_vm;
@@ -93,7 +143,7 @@ stdin_enumerator(task_closure closure, void *data)
 }
 
 static void
-stdin_readable(int fd, void *data)
+stdin_readable(int fd UNUSED_, void *data)
 {
     char buffer[1000];
     int n;
@@ -145,14 +195,25 @@ stdin_suspender(vm the_vm, void *data)
 }
 
 static package
-bf_read_stdin(Var arglist, Byte next, void *vdata, Objid progr)
+bf_read_stdin(Var arglist UNUSED_, Byte next UNUSED_, void *vdata UNUSED_, Objid progr UNUSED_)
 {
     stdin_waiter *w = mymalloc(sizeof(stdin_waiter), M_TASK);
 
     return make_suspend_pack(stdin_suspender, w);
 }
+
+/*\ Again, something that only *this* file does because we are
+ *| do not want to have actual content.  Also never forget to comment
+ *| your #endifs if the corresponding #ifs are more than 1 screen above.
+ */
 #endif				/* EXAMPLE */
 
+/*\ If the file defines a registration function, it should be the last
+ *| thing, or, at least, that's where people will expect to find it.
+ *| Also, a file will typically only need one of these, though if
+ *| there are weird #ifdef situations, then fine; do what you need to.
+ *| Alsoalso, notice how experiments.h does not have this declaration.
+ */
 void
 register_experiments(void)
 {
@@ -162,6 +223,11 @@ register_experiments(void)
 #endif
 }
 
+
+/*\ Finally, if when writing your function you went back in time to,
+ *| say, 1996, then maybe you'd have CVS log entries showing up here, too.
+ *| Otherwise, you won't.
+ */
 
 /*
  * $Log$
